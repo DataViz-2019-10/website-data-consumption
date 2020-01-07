ad6ec09f64e7c4f62e5942ac2bb58f28af98cb0a
Gary SUBLET <gary.sublet@hotmail.fr>
2020-01-07 11:08:57 +0100

    mise à jour du script de tests

--- test_har_valide.py
+++ test_har_valide.py
@@ -1,9 +1,21 @@
 import json
+import os
 
-with open("raw_data/Video/YouTube.har", 'r') as f:
-        data = json.load(f)
+export_folder = "exports"
 
-if data['log']:
-	print("A priori c'est ok")
-else:
-	print("Ya surement un soucis")
+for folder_name in os.listdir(export_folder):
+    for file_name in os.listdir(os.path.join(export_folder + "/" + folder_name)):
+        if file_name.endswith(".har"):
+            file_loc = os.path.join(export_folder + "/" + folder_name, file_name)
+            print(file_loc)
+            try:
+                with open(file_loc, 'r') as f:
+                        data = json.load(f)
+            except:
+                print("c'est mort")
+                continue
+
+            if data['log']:
+            	print("A priori c'est ok")
+            else:
+            	print("Ya surement un soucis")
